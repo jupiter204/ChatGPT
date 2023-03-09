a026975ec2416669ae631eb4397f72faabeb3aa3
jupiter <ss0922071773@gmail.com>
2023-03-09 20:02:53 +0800

    beta 1.1 version

--- bot.py
+++ bot.py
@@ -38,6 +38,6 @@ async def talk(ctx):
     print(f'{ctx.author} have a require')
     message = ctx.message.content
     user_message = "" + message[6 : len(message)]
-    await ctx.reply(f'{chat_prompt(level,user_message)}   :from ChatGPT')
+    await ctx.reply(f'{chat_prompt(level,user_message)}\n:from ChatGPT')
 
 bot.run('OTczMTQ1MDkyOTgwMzc1NTUy.GeG4tf.0dZKUVRLTBixpBXqF47EL6dzMMmR7wf6VUQb6U')
