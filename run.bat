4ebd53944f2e008ed330c306b3f51914a82662f6
jupiter <ss0922071773@gmail.com>
2023-03-13 19:01:58 +0800

    beta 1.2.5

--- run.bat
+++ run.bat
@@ -1,9 +1,9 @@
 rem 1.2.5
 @echo off
-pip install requests
+python -m pip install requests
 python update.py
-pip install python_dotenv
-pip install discord.py
-pip install openai
+python -m pip install python_dotenv
+python -m pip install discord.py
+python -m pip install openai
 python bot.py
 pause
