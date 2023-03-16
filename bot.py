6b16a0dcb6fd62512c061597bfd0531a2d6ecada
jupiter <ss0922071773@gmail.com>
2023-03-16 19:48:42 +0800

    beta 1.3

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ async def thelp(ctx):
     embed=discord.Embed(title="help list", color=0x1494f5,timestamp=datetime.now(timezone(timedelta(hours=+8))))
     embed.set_author(name="chatBot", icon_url="https://cdn.discordapp.com/app-icons/973145092980375552/da80b4dc5988b53c06461209dd2193c5.png")
     embed.add_field(name="+talk", value="和ChatGPT聊天", inline=False)
-    embed.add_field(name="+thelp", value="ChatGPT產生一張描述的照片(很貴少用一點)", inline=False)
+    embed.add_field(name="+image", value="ChatGPT產生一張描述的照片(很貴少用一點)", inline=False)
     embed.add_field(name="+set", value="設定機器人溫度0.0~1.0", inline=False)
     embed.add_field(name="+now", value="顯示目前機器人溫度，預設0.8", inline=False)
     embed.add_field(name="+thelp", value="顯示此列表", inline=False)
